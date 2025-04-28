ef45b823e034d487d06a05b403aa976d9f344023
Todd Kamin <toddkamin@hotmail.com>
2025-04-28 16:35:18 -0600

    fix issue on vercel??

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,8 +2,12 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchSellers, fetchInvoiceById } from '@/app/lib/data';
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
   const [invoice, sellers] = await Promise.all([
     fetchInvoiceById(id),
